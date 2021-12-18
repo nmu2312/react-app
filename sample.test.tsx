00883acdeef31c00e8f4c20e71d6b39b9036559a
nmu2312 <nmu2312@gmail.com>
2021-12-19 01:01:05 +0900

    change front

--- sample.test.tsx
+++
@@ -1,5 +0,0 @@
-describe('isZipCode()', () => {
-  test('sample', () => {
-    expect(1 === 1 * 1).toBe(true);
-  });
-});
--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>UPDATED!!</p>
+        <p>HELLO CI/CD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
