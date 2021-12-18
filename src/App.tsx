ff8e00b28065840be645a25de1f8a6e8163e8b85
nmu2312 <nmu2312@gmail.com>
2021-12-18 23:23:41 +0900

    feature

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO !! CICD Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>UPDATED!!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
